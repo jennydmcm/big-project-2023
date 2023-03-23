10df2f6ed5534b77f1fbfad8f8430e3467f30d73
jennydmcm <jenniferdmcmahon@gmail.com>
2023-03-23 15:27:18 -0700

    components folders added with templates

---
+++ components/logo/index.js
@@ -0,0 +1,9 @@
+import { useState } from "react"
+import styles from "/logo.module.css"
+
+
+export default function Logo() {
+    return (
+        <></>
+    )
+}
---
+++ components/slider/index.js
@@ -0,0 +1,9 @@
+import { useState } from "react"
+import styles from "/slider.module.css"
+
+
+export default function Slider() {
+    return (
+        <></>
+    )
+}
