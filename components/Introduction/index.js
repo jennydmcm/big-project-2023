8d5927ca0dfa56b91d7c7aceacf22ad13539c606
linaaa <113660013+carolinalxu@users.noreply.github.com>
2023-05-16 18:10:50 -0700

    fixing introduction

--- components/Introduction/index.js
+++ components/Introduction/index.js
@@ -1,19 +1,30 @@
 import { useState, useEffect } from 'react';
-import styles from '@/components/Introduction/Introduction.module.css'
+import styles from '@/components/Introduction/Introduction.module.css';
 
-const IntroductionModal = ({ show, handleCloseModal }) => {
+const IntroductionModal = ({ handleCloseModal }) => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const hasShownModal = localStorage.getItem('hasShownModal');
+    if (!hasShownModal) {
+      setShow(true);
+      localStorage.setItem('hasShownModal', true);
+    }
+  }, []);
 
   return (
     <>
       {show && (
-        <div className= {styles.modal-container}>
+        <div className={styles.modalContainer}>
           <div className={styles.modal}>
-            <button className={styles.close-button} onClick={handleCloseModal}>
+            <button className={styles.closeButton} onClick={handleCloseModal}>
               X
             </button>
-            <img src="/girl.svg" alt="Girl" className={styles.modal.img} />
-            <h3 className={styles.modal-heading}>Welcome to our Naloxone web app!</h3>
-            <p className={styles.modal-text}>This app is designed to help individuals learn about and access naloxone, a medication used to reverse opioid overdoses. We hope you find it helpful!</p>
+            <img src="/girl.svg" alt="Girl" className={styles.modalImg} />
+            <h3 className={styles.modalHeading}>Welcome to our Naloxone web app!</h3>
+            <p className={styles.modalText}>
+              This app is designed to help individuals learn about and access naloxone, a medication used to reverse opioid overdoses. We hope you find it helpful!
+            </p>
           </div>
         </div>
       )}
